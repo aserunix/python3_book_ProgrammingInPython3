ad7f3cdcb5040b6f86f34d9b628c29c7fa40149e
aser <aserunix@gmail.com>
2018-09-25 19:12:20 +0800

    add file 3_4_1

--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-import collections
 
 # i=1
 # s="1"
@@ -13,7 +12,16 @@ import collections
 # for x,y in ((1,1),(2,4),(3,9)):
 #     print(x,y)
 
-Sale=collections.namedtuple("Sale","productid customerid date quantity price")
-sales=[]
-sales.append(Sale(432,921,"2008-09-14",3,7.99))
-sales.append(Sale(419,874,"2008-09-15",1,18.49))
+# import collections
+# Sale=collections.namedtuple("Sale","productid customerid date quantity price")
+# sales=[]
+# sales.append(Sale(432,921,"2008-09-14",3,7.99))
+# sales.append(Sale(419,874,"2008-09-15",1,18.49))
+
+# for a in range(1,5):
+#     print(a)
+
+for t in zip(range(4),range(0,10,2),range(1,10,2)):
+    print(t)
+
+
